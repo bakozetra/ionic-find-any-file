f166925ffacb8913b39c71b9b947ab58f36a9985
Synthia <synthia.han@onja.org>
2022-09-23 16:24:49 +0300

    Adding hide to all columns (23 of Sept)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,7 +3,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
 export interface Data {
   movies: string;
 }
-
+const INITIALCOLUMNS = [
+  { name: 'Name', hidden: false },
+  { name: 'Company', hidden: false },
+  { name: 'Genre', hidden: false },
+  { name: 'Image', hidden: false },
+];
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -13,13 +18,15 @@ export interface Data {
 export class Tab2Page {
   public data: Data;
   public columns: any;
+  public tempColumns: any;
   public rows: any;
   constructor(private http: HttpClient) {
+    this.tempColumns = INITIALCOLUMNS;
     this.columns = [
-      { name: 'Name' },
-      { name: 'Company' },
-      { name: 'Genre' },
-      { name: 'Image' },
+      { name: 'Name', hidden: false },
+      { name: 'Company', hidden: false },
+      { name: 'Genre', hidden: false },
+      { name: 'Image', hidden: false },
     ];
     this.http.get<Data>('../../assets/movies.json').subscribe((res) => {
       console.log(res, 'resss');
@@ -27,9 +34,21 @@ export class Tab2Page {
     });
   }
   isToggle = false;
-
   toggleme(e) {
-    this.isToggle = !this.isToggle;
+    console.log('columns[0]::::::', this.columns[0]);
+
+    console.log('this.tempColumns::::::', this.tempColumns);
+    const updateColumns = this.tempColumns.map((a) => {
+      if (a.name === e) {
+        console.log('e::::::', e);
+        console.log('a.name::::::', a.name);
+        a.hidden = !a.hidden;
+      }
+      return a;
+    });
+    console.log('updateColumns:::::: down', updateColumns);
+    this.columns = updateColumns.filter((c) => !c.hidden);
+    // this.isToggle = !this.isToggle;
   }
   onSort(event) {
     console.log(event);
