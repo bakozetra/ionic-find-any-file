b8379d61432ab4f3ff73968752054bf28b33e293
Synthia <synthia.han@onja.org>
2022-09-30 12:00:29 +0300

    Adding the draggable item into local storage(30th of Sept)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -55,6 +55,9 @@ export class Tab2Page implements OnInit {
     if (this.getLocalStoragSort() !== null) {
       this.sortOrder = this.getDataRowSort();
     }
+    if (this.getLocalStoragDrag !== null) {
+      this.columns = this.getDataRowDrag();
+    }
 
     if (this.getLocalStorageRow() !== null) {
       this.rows = JSON.parse(this.getLocalStorageRow());
@@ -148,7 +151,18 @@ export class Tab2Page implements OnInit {
   }
   test: any = [];
   rearrange(event) {
-    console.log('event::::::', event);
+    const arr = this.array_move(this.columns, event.prevValue, event.newValue);
+    this.setLocalStorageDag(arr);
+  }
+  array_move(arr, old_index, new_index) {
+    if (new_index >= arr.length) {
+      var k = new_index - arr.length + 1;
+      while (k--) {
+        arr.push(undefined);
+      }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr;
   }
 
   updateValue(event, cell, rowIndex) {
